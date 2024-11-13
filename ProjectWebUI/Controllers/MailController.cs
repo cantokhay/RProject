7c72909c9c0117e8d71f9d8328e16a80c82b7c1c
Can Tokhay <cantokhay@gmail.com>
2024-11-13 14:37:50 +0300

    Section 27

---
+++ ProjectWebUI/Controllers/MailController.cs
@@ -0,0 +1,42 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using ProjectWebUI.VMs.MailVM;
+
+namespace ProjectWebUI.Controllers
+{
+    public class MailController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(CreateMailVM createMailVM)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+            MailboxAddress from = new MailboxAddress("Rezervasyon Bilgilendirme", "Mail Address");
+            mimeMessage.From.Add(from);
+            MailboxAddress to = new MailboxAddress("User", createMailVM.ReceiverMail);
+            mimeMessage.To.Add(to);
+
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody = createMailVM.Body;
+            mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+            mimeMessage.Subject = createMailVM.Subject;
+
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.Connect("smtp.gmail.com", 587, false);
+            smtpClient.Authenticate("Mail Address", "key");
+
+            smtpClient.Send(mimeMessage);
+            smtpClient.Disconnect(true);
+
+
+            return RedirectToAction("Index", "Category");
+        }
+    }
+}
---
+++ ProjectWebUI/VMs/MailVM/CreateMailVM.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectWebUI.VMs.MailVM
+{
+    public class CreateMailVM
+    {
+        public string ReceiverMail { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
