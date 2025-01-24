1b4a82898a75c072fd53a78d6ba1c224a3bcf1ff
Can TOKHAY <cantokhay@outlook.com>
2025-01-24 11:40:22 +0300

    End of 32 RapidAPI

--- ProjectWebUI/Controllers/RapidAPIController.cs
+++ ProjectWebUI/Controllers/RapidAPIController.cs
@@ -1,14 +1,34 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ProjectWebUI.VMs.RapidAPIVM;
 
 namespace ProjectWebUI.Controllers
 {
     [AllowAnonymous]
     public class RapidAPIController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://tasty.p.rapidapi.com/recipes/list?from=0&size=40&tags=under_30_minutes"),
+                Headers =
+                {
+                    { "x-rapidapi-key", "56eeaae0a2msh1f5a78362b65e64p1c49efjsn933601bf2799" },
+                    { "x-rapidapi-host", "tasty.p.rapidapi.com" },
+                },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var root = JsonConvert.DeserializeObject<RootTastyVM>(body);
+                var values = root.Results;
+                return View(values.ToList());
+            }
         }
     }
 }
---
+++ ProjectWebUI/VMs/RapidAPIVM/ResultTastyVM.cs
@@ -0,0 +1,15 @@
+﻿namespace ProjectWebUI.VMs.RapidAPIVM
+{
+    public class RootTastyVM
+    {
+        public List<ResultTastyVM> Results { get; set; }
+    }
+
+    public class ResultTastyVM
+    {
+        public string Name { get; set; }
+        public string original_video_url { get; set; }
+        public int prep_time_minutes { get; set; }
+        public string thumbnail_url { get; set; }
+    }
+}
